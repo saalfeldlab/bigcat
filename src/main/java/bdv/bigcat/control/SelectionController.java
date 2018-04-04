ee84e9d3bb7fae6c88ec9f13c0a96212c627a60a
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2018-04-04 19:36:20 -0400

    remove obsolete clear cache

--- src/main/java/bdv/bigcat/control/SelectionController.java
+++ src/main/java/bdv/bigcat/control/SelectionController.java
@@ -114,7 +114,6 @@ public class SelectionController
 	{
 		activeFragmentId = id;
 		colorStream.setActive( id );
-		colorStream.clearCache();
 		System.out.println( "activeID = " + activeFragmentId );
 	}
 
